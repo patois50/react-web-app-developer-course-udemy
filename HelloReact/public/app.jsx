a386a12de95d52931eb83ad1fbadf5f8a092aaba
Patrick McGeever <patois50@gmail.com>
2017-06-18 10:54:44 +0100

    3.12: Simple hello world

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -1,6 +1,27 @@
-var Greeter = React.creat
+var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is from a component'
+    };
+  },
+  render: function () {
+    var name = this.props.name;
+    var message = this.props.message;
+
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
+      </div>
+    );
+  }
+});
+
+var firstName = 'Patrick';
+var someMessage = 'This is a neat message';
 
 ReactDOM.render(
-  <h1>Hello React!</h1>,
+  <Greeter name={firstName} message={someMessage}/>,
   document.getElementById('app')
 );
